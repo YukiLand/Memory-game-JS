147a01ed9ab6aae474d2bc781f2de203e044c537
Chauvac Henri <chenri@linux.home>
2020-04-29 12:11:49 +0200

    rand + flipp system ok

--- js/game.js
+++ js/game.js
@@ -1,6 +1,13 @@
+let score = 0;
+let count = document.querySelector(".score");
+let mult = 1;
+let bonus = document.querySelector(".bonus");
 let carte = document.getElementsByClassName("carte");
-const cartes = [...card];
+const cartes = [...carte];
 const deck = document.getElementById("deck-carte");
+let sameCartes = document.getElementsByClassName("same");
+let closeIcon = document.querySelector(".close");
+var choice = [];
 
 function randCartes(array) {
     var index = array.length;
@@ -21,27 +28,114 @@ window.onload = start();
 
 function start() {
     var cartesRand = randCartes(cartes);
-
     for (var i = 0; i < cartesRand.length; i++) {
+        deck.innerHTML = "";
+        [].forEach.call(cartes, function(item) {
+            deck.appendChild(item);
+        });
+        cartes[i].classList.remove("show", "open", "match", "disabled");
+    }
+    score = 0;
+    count.innerHTML = score;
+    mult = 1;
+    bonus.innerHTML = 'x' + mult;
 
+
+    seconde = 0;
+    minute = 0;
+    let timer = document.querySelector(".timer");
+    timer.innerHTML = "0:00";
+    clearInterval(intervale);
+}
+
+function countScoreBonus() {
+    score += (1 * mult);
+    mult++;
+    count.innerHTML = score;
+    bonus.innerHTML = mult;
+    if (score == 1) {
+        seconde = 0;
+        minute = 0;
+        timerStart();
     }
 }
 
 var seconde = 0;
 var minute = 0;
-var heure = 0;
 var timer = document.querySelector(".timer");
 var intervale;
 
 function timerStart() {
-    intervale = setInterval(interval(), 1000);
+    intervale = setInterval(clock(), 1000);
 }
 
-function interval() {
+function clock() {
     timer.innerHTML = minute + ':' + seconde;
-    seconde++;
+    seconde++;;
     if (seconde == 60) {
         minute++;
         seconde = 0;
     }
 }
+
+var dispCarte = function() {
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+};
+
+function flipCarte() {
+    choice.push(this);
+    var flips = choice.length;
+
+    if (flips === 2) {
+        countScoreBonus();
+        if (choice[0].type === choice[1].type) {
+            same();
+        } else {
+            notSame();
+        }
+    }
+}
+
+function same() {
+    choice[0].classList.add("match", "disabled");
+    choice[1].classList.add("match", "disabled");
+    choice[0].classList.remove("show", "open", "no-event");
+    choice[1].classList.remove("show", "open", "no-event");
+    choice = [];
+}
+
+function notSame() {
+    choice[0].classList.add("unmatched");
+    choice[1].classList.add("unmatched");
+    disabled();
+    setTimeout(function() {
+        choice[0].classList.remove("show", "open", "no-event", "unmatched");
+        choice[1].classList.remove("show", "open", "no-event", "unmatched");
+        enable();
+        choice = [];
+        }, 1100);
+
+}
+
+function disabled() {
+    Array.prototype.filter.call(cartes, function(carte) {
+        carte.classList.add("disabled");
+    });
+}
+
+function enable() {
+    Array.prototype.filter.call(cartes, function(carte) {
+        carte.classList.remove("disabled");
+        for (var i = 0; i < sameCartes.length; i++) {
+            sameCartes[i].classList.add("disabled");
+        }
+    });
+}
+
+for (var i = 0; i < cartes.length; i++) {
+    carte = cartes[i];
+    carte.addEventListener("click", dispCarte);
+    carte.addEventListener("click", flipCarte);
+};
